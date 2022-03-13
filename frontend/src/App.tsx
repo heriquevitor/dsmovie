6173433f775f6c32d132b289c6f082792423b919
Herique <heriquevitor3@gmail.com>
2022-03-13 15:47:57 -0300

    Form Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,  /*  Aqui ficam os componentes de rotas da biblioteca do from abaixo*/
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
-);
+     /* Aqui você inicia a rota, e a navbar ta ai pq ela vai estar em tdas*/
+     /* O routes vai configurar rota por rota  */
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
